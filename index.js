cbc510e56349a79337cce154052d56ead7e0ad4d
Chris Lin <chrislin@utexas.edu>
2018-02-22 13:07:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+
+
+function destructivelyAppendKitten(name) {
+  kittens = kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens = kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens = kittens.pop();
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens = kittens.shift();
+}
+
+function appendKitten(name) {
+  kittens.push(name);
+}
+
+function prependKitten(name) {
+  kittens.unshift(name);
+}
+
+function removeLastKitten() {
+  kittens.pop();
+}
+
+function removeFirstKitten() {
+  kittens.shift();
+}
+
